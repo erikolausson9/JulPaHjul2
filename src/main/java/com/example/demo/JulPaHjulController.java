8cb6dc714d62d2637fc0eecbc6131088447c57ad
feliciahaggqvist <felicia.haggkvist@hotmail.com>
2019-11-20 15:39:18 +0100

    Created login viewer and a viewer for adding restaurants

--- src/main/java/com/example/demo/JulPaHjulController.java
+++ src/main/java/com/example/demo/JulPaHjulController.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class JulPaHjulController {
@@ -10,6 +14,25 @@ public class JulPaHjulController {
     String goToIndexPage(){
 
         return "index";
+    }
+
+
+
+
+
+
+    @GetMapping("/login")
+    String getLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    String postLogin(HttpSession session, @RequestParam String username, @RequestParam String password) {
+        if (username.equals("admin") && password.equals("12345")) {
+            session.setAttribute("username", username);
+            return "index";
+        }
+        return "login";
     }
 
 }
