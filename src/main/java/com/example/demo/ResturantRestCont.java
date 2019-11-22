7cb6bb1d524c1d714bb2926e1bd171e6d37fc5be
SimonDDG <simon.douchka@gmail.com>
2019-11-22 11:35:46 +0100

    Little cleaner ;)

--- src/main/java/com/example/demo/ResturantRestCont.java
+++ src/main/java/com/example/demo/ResturantRestCont.java
@@ -20,32 +20,5 @@ public class ResturantRestCont {
         return repository.getRestaurantList();
     }
 
-    @PostMapping("/mapChange")
-    Position focusMap(@RequestParam String mapChange){
-        switch(mapChange){
-            case "GamlaVaster":
-                return(new Position(55.604640, 12.996836));
-            case "Carolikvarteren":
-                return(new Position(55.606245, 13.009344));
-            case "Davidshall":
-                return(new Position(55.598955, 12.998516));
-            case "Slottsstaden":
-                return(new Position(55.597246, 12.976060));
-            case "Mollevangen":
-                return(new Position(55.591482, 13.008791));
-            case "Radmansvangen":
-                return(new Position(55.593009, 13.001743));
-            case "Slussen":
-                return(new Position(55.607810, 13.016972));
-            case "Varnhem":
-                return(new Position(55.605576, 13.022763));
-            default:
-                System.out.println("Switch case funkade inte i rest-controller");
-                return(new Position(55.604640, 12.996836));
-
-        }
-
-    }
-
 
 }
