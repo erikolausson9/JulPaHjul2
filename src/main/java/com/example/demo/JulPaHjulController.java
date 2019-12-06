07578f28d061daca92c69de1100f1a531a6a055c
SimonDDG <simon.douchka@gmail.com>
2019-12-06 09:18:09 +0100

    Städat och laggt til lcontroller + html temlate för mina bookningar, samt en länk

--- src/main/java/com/example/demo/JulPaHjulController.java
+++ src/main/java/com/example/demo/JulPaHjulController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice;
 
 import javax.servlet.http.HttpSession;
@@ -162,6 +159,12 @@ public class JulPaHjulController {
         model.addAttribute("restaurant", serviceLayer.getRestaurant(id));
         model.addAttribute("id", id);
     return "view";
+    }
+
+    @GetMapping("/myBookings/{username}")
+    String getBookings (@PathVariable String username){
+
+        return "myBookings";
     }
 
 }
